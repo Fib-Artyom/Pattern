46a3d6ebc7b570f054dfc166c0856d34390f79e4
Fib-Artyom <73397850+Fib-Artyom@users.noreply.github.com>
2020-10-24 22:22:08 +0300

    Add files via upload

---
+++ Price/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+/*Есть текст с ценами в рублях.
+Извлечь из него цены. 
+– пример правильных выражений:
+23.78 руб.
+– пример неправильных выражений:
+22 р., 0.002 руб.*/
+
+namespace Price
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            string Price = "23.78 руб.";           
+            if (Price.Regex())
+            {
+                Console.WriteLine("true");
+            }
+            else Console.WriteLine("false");
+            Console.ReadKey();
+        }
+    }
+    public static class StringExtension
+    {
+
+        public static bool Regex(this string str)
+        {
+            string Pattern = @"(\w+ |^()| )[\d]+\.[\d]{2} (руб.)";
+            Match match = System.Text.RegularExpressions.Regex.Match(str, Pattern);
+            if (match.Success)
+            {
+                return true;
+            }
+            else return false;
+
+        }
+    }
+}
